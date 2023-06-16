63e3c4ed41176f849e293f57ce2f40ad550daa3d
netweaver <developer.oduwa@gmail.com>
2023-06-16 03:31:27 +0100

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 *main -  main entry point
-*
 *Return: 0 on success, non-zero on failure
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
