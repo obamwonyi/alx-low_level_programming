4da2c47feb508b90eb678666ed8f27f437c694a8
netweaver <developer.oduwa@gmail.com>
2023-06-16 03:26:58 +0100

    solved low level programming task 4 finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 *main -  main entry point
-*return - always 0 (success)
+*
+*Return: 0 on success, non-zero on failure
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
