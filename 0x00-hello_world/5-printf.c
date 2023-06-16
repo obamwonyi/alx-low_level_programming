b1f8a96bec169abaa28ae4c5967d4a52cfb07ee3
netweaver <developer.oduwa@gmail.com>
2023-06-16 03:43:41 +0100

    solved low level programming task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main function
+*Return: 0 on success , non zero on failure
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
