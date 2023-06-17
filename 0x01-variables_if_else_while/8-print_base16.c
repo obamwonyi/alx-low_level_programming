5b973c9f2a6c7c54d50a83994fc4da8094138fd7
netweaver <developer.oduwa@gmail.com>
2023-06-17 04:33:54 +0100

    solved variable,if/else,while task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry Point
+*Description: 'print alphabets except q and a'
+*Return: always 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; m <= 102; n++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
