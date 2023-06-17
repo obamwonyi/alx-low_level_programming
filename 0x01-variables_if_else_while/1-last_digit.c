5151760d016480981d942c71583b2fe88eeab2e7
netweaver <developer.oduwa@gmail.com>
2023-06-17 03:43:17 +0100

    fixed variable,if/else,while task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less 6 and not 0\n", n, ld);
+		printf("Last digit of %d is %d and is less 6 and not 0\n", n, ld);
 	}
 	return (0);
 }
