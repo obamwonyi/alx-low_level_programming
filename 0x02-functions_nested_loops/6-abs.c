bd2f69b3b41de76bbd88afe5b9ea8d1656e6e61c
netweaver <developer.oduwa@gmail.com>
2023-06-21 03:23:58 +0100

    solved function and nested loops task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_abs - estimate the absolute value
+*@x: reserve the number to be estimated
+*Return: absolute value of the integer
+*/
+int _abs(int x)
+{
+	if(i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
