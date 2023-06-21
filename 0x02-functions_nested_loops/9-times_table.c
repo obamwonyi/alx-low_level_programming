8917606b26ca8aaeb1d5927f404f626e924669bf
netweaver <developer.oduwa@gmail.com>
2023-06-21 15:24:02 +0100

    solved function and nested loops task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,14 +24,14 @@ void times_table(void)
 			}
 			else
 			{
-				if (y != 0)
+				if (b != 0)
 				{
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
 				}
 			}
-			_putchar(z + '0');
+			_putchar(c + '0');
 		}
 		_putchar('\n');
 	}
