730295300fc2c55eb58967027d3d6cf914bbacf8
netweaver <developer.oduwa@gmail.com>
2023-06-21 02:24:19 +0100

    solved function and nested loops task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_alphabet - print alphabet
+*Description: print all the alphabet in lowercase.
+*Return: None.
+*/
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{	
+       	 	int i = 0;
+
+        	for (i = 97; i < 123; i++)
+        	{
+                	_putchar(i);
+        	}
+        	_putchar('\n');
+
+		x++;
+	}
+}
