e5667a9cbdbb88a1ed7bffa479c8ad46af938c58
netweaver <developer.oduwa@gmail.com>
2023-06-21 05:02:32 +0100

    solved function and nested loops task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*add - adds two integer
+*Return: int
+*/
+int add(int i, int x)
+{
+	return (i + x)
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+*times_table - prints out the times table
+*Return: None
+*/
+void times_table(void)
+{
+
