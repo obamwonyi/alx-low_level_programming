1728d5337023532f2a2f3a90994e9e7020f4f145
netweaver <developer.oduwa@gmail.com>
2023-06-21 03:55:52 +0100

    solved function and nested loops task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_last_digit
+*@x: number to check for last digit
+*Return: value of last digit of x
+*/
+int print_last_digit(int x)
+{
+	int lastDigit = x % 10;
+
+	if(lastDigit < 0)
+	{
+		lastDigit = -1 * lastDigit;
+		_putchar(lastDigit);
+		return (0);
+	}
+	_putchar(lastDigit);
+	return (0);
+}
