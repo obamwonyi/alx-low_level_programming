6c5c7c4589565ab66d2d4ea62b6e22fdc79d98e8
netweaver <developer.oduwa@gmail.com>
2023-06-21 02:55:25 +0100

    solved function and nested loops task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*int _islower - check for lowercase letters
+*Return: 1 on success, 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
