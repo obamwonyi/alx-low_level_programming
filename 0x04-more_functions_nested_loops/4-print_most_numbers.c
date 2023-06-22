32be214d44d28c1fdfd85d7ef31b174bf04a90cb
netweaver <developer.oduwa@gmail.com>
2023-06-23 00:52:13 +0100

    solved more functions and nested loops task 3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_most_numbers - prints int 0 - 9 , excluding 2 and 4
+*Return: None
+*/
+void print_most_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		if ((a != 52) || (a != 50))
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
