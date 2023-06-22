18675cdcccd3a360d235ee47e27f406f61cbcd58
netweaver <developer.oduwa@gmail.com>
2023-06-23 00:07:30 +0100

    solved more functions and nested loops task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+*mul - return the product of two integers
+*@a: integer one (to multiply two)
+*@b: integer two (to by multiplied by one)
+*Return: value of the product 
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
