2b8fb9609ebab2df47a24b82584ef1d32945ade0
netweaver <developer.oduwa@gmail.com>
2023-06-23 05:51:27 +0100

    solved more functions and nested loops task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,19 +7,19 @@
 void print_triangle(int size)
 {
 	int i, j;
-    
-	for (i = 1; i <= size; i++) 
+
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= size - i; j++) 
+		for (j = 1; j <= size - i; j++)
 		{
 			_putchar(' ');
 		}
-        
-		for (j = 1; j <= i; j++) 
+
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
-        
+
 		_putchar('\n');
-    }
+	}
 }
