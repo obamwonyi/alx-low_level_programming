0ff8be3d1c6b866886e10535aa94f88f26c3c7f3
netweaver <developer.oduwa@gmail.com>
2023-06-23 02:59:26 +0100

    solved more functions and nested loops task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - print out lines
+*@n: numbers of line to be printed
+*Return: None
+*/
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+}
