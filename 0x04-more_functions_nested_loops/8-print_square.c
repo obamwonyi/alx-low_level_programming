5830ab9969dfa509af982c03df481eb6736434a3
netweaver <developer.oduwa@gmail.com>
2023-06-23 04:02:25 +0100

    solved more functions and nested loops task 7

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_square - print out square
+*@size: size of the square
+*Return: None
+*/
+void print_square(int size)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < size; i++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
