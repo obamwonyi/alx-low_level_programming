83faaf72a3257cb16b6c9855206bed7f6e86a4e9
netweaver <developer.oduwa@gmail.com>
2023-06-27 01:51:41 +0100

    solved pointers, arrays and strings task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+* reset_to_98 - reset integers passed as arguments
+* @n: integer
+* Return: None
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the readme for pointers, arrays and string
