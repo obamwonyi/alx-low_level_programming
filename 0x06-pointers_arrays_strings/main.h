9ace1eb42ec1d13c1a3ea24987544d7563091e58
netweaver <developer.oduwa@gmail.com>
2023-07-01 00:02:38 +0100

    solved more pointers, arrays and strings task 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,4 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
-char *_strncpy(char *dest, char *src, int n);:
+char *_strncpy(char *dest, char *src, int n);
