6aeead6362b61216507dc81a009fccab61beea1a
netweaver <developer.oduwa@gmail.com>
2023-07-01 00:27:11 +0100

    solved more pointers, arrays and strings task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
 * _strncpy - returns copied string
 *@dest: destination of the copied string
 *@src: source of the copied
+*@n: the limit (number of chars to copy to dest)
 *Return: char * ( in form of string)
 */
 char *_strncpy(char *dest, char *src, int n)
