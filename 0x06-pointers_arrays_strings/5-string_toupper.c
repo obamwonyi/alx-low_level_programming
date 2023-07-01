f8d1fe9af47aa4e93504a71d05e702230c902b98
netweaver <developer.oduwa@gmail.com>
2023-07-01 02:30:50 +0100

    solved more pointers, arrays and strings task 4

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*string_toupper - convert lower case letters to uppercase
+*Return: char
+*/
+char *string_toupper(char *str)
+{
+
+	int i;
+	int hold;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		/**
+		* if they are lower case laters (according to ASCII code)
+		* execute code block
+		*/
+		if (((int)str[i] > 96) && ((int)str[i] < 123))
+		{
+			hold = (int)str[i] - 32;
+			str[i] = (char)hold;
+		}
+	}
+
+	return (str);
+}
