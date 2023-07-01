0a2753f8dd576851aa4d510c2ba152bc7e95d7c1
netweaver <developer.oduwa@gmail.com>
2023-07-01 04:20:52 +0100

    solved more pointers, arrays and strings task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*leet - encodes string into 1337
+*@str: string
+*Return: string
+*/
+char *leet(char *str)
+{
+	char *copy = str;
+	char key[] = {'A','E','O','T','L'};
+	int value[] = {4,3,0,7,1};
+
+	unsigned int i;
+
+	while (*str)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if(*str == key[i] || *str == key[i] + 32)
+			{
+				*str = 48 + value[i];
+			}
+		}
+		str++;
+	}
+	return (copy);
+}
