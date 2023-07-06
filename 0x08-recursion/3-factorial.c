ad789b5e4aae6aed45ca9beec34e26717acd53c3
netweaver <developer.oduwa@gmail.com>
2023-07-06 03:57:36 +0100

    solved recursion task 2

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*factorial
+*@n: number who's factorial is to be calculated
+*Return: final value, or -1
+*/
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}
+	else if (n < 0)
+	{
+		return -1;
+	}
+	return n * factorial(n - 1);
+}
+
