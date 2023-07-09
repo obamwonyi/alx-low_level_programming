a7de7c6d380a8afc2852ef5e39ba39e054c6a33e
netweaver <developer.oduwa@gmail.com>
2023-07-09 13:07:41 +0100

    solved argc, argv 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry Point
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0 on success, 1 on failure
+*/
+int main(int argc, char *argv[])
+{
+	int count, totalSum, num;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (count = 1; count < argc; count++)
+		{
+			num = atoi(argv[count]);
+
+			if (num == 0 && argv[count][0] != '0')
+			{
+				printf("Error\n");
+				return (0);
+			}
+			totalSum += num;
+		}
+	}
+
+	printf("%d\n", totalSum);
+	return (0);
+}
