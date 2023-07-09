001da464e9461b66b5047eb5de53a3d0c083d9c0
netweaver <developer.oduwa@gmail.com>
2023-07-09 11:22:43 +0100

    solved argc, argv 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - Entry Point
 *@argc: number arguments
@@ -7,10 +8,17 @@
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if ((argc < 3) | (argc > 3))
 	{
 		printf("Error\n");
+		return (1);
 	}
 
-	return (argv[1] * argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+
+	return (0);
 }
