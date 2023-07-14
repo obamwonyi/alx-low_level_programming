89d7e1339a5978a3ebb898d7e7ea2bcbfc733151
netweaver <developer.oduwa@gmail.com>
2023-07-14 17:38:46 +0100

    solved more malloc, free task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,43 +15,23 @@ char *string_nconcat(char *str1, char *str2, unsigned int num)
 	unsigned int str2Length = 0;
 
 	if (str1 == NULL)
-	{
 		str1 = "";
-	}
 	if (str2 == NULL)
-	{
 		str2 = "";
-	}
-
 	while (str1[str1Length] != '\0')
-	{
 		str1Length++;
-	}
-
 	while (str2[str2Length] != '\0')
-	{
 		str2Length++;
-	}
-
 	str = malloc(str1Length + num + 1);
-
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-
-
 	for (i == 0; str1[i] != '\0'; i++)
-	{
 		str[i] = str1[i];
-	}
-
 	for (j = 0; j < num; j++)
 	{
 		str[i] = str2[j];
 		j++;
 	}
-
 	str[i] = '\0';
 	return (str);
 }
