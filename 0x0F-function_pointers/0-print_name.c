52c0e810a45c968f5cbe8841d79085e3563ee93b
netweaver <developer.oduwa@gmail.com>
2023-07-19 22:51:23 +0100

    solved 0x0F.C-Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+print_name - print name
+*@name: name to be printed
+*@f: function that handles the name printing
+*Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
