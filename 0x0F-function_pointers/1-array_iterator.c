ff8784c70c8d81727cbcfbd9dd354f32bf50e47a
netweaver <developer.oduwa@gmail.com>
2023-07-19 23:41:06 +0100

    solved 0x0F.C-Function pointers 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 	{
-		while (array <= end)
+		while (array <= stop)
 		{
 			action(*array++);
 		}
