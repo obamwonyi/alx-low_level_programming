5f08e8a975a36fd541768a49288e786ac67736a0
netweaver <developer.oduwa@gmail.com>
2023-07-19 23:19:23 +0100

    solved 0x0F.C-Function pointers 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+*array_iterator - maps an array through a func pointer
+*@array: array of integers
+*@size: size of the array
+*@action: function pointer
+*Return: None (void)
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *stop = (size - 1) + array;
+
+	if (array && size && action)
+	{
+		while (array <= end)
+		{
+			action(*array++);
+		}
+	}
+}
