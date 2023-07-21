6eb7f64d71c72e964dbce29cd7df318b1828db87
netweaver <developer.oduwa@gmail.com>
2023-07-21 23:46:07 +0100

    solved 0x10. C - Variadic functions task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,9 +11,9 @@
 typedef struct token
 {
 	char *token;
-	void (*f)(char *. va_list);
+	void (*f)(char * . va_list);
 } token_t;
- 
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
