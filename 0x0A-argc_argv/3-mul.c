c8675a2d6a79909c1108ad8380cba64f98f06331
netweaver <developer.oduwa@gmail.com>
2023-09-02 02:39:21 +0100

    0x0A. C - argc, argv task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1,num2 = 0;
+	int num1, num2 = 0;
 
 	if (argc < 3)
 	{
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = atoi(argv[argc-2]);
-	num2 = atoi(argv[argc-1]);
+	num1 = atoi(argv[argc - 2]);
+	num2 = atoi(argv[argc - 1]);
 
 	printf("%d\n", (num1 * num2));
 
