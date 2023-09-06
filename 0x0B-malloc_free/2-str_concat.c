3de5fb51152226f3cc606d4cc4056e9dee9a2e46
netweaver <developer.oduwa@gmail.com>
2023-09-06 04:11:50 +0100

    0x0B. C - malloc, free task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
 /**
 * _strlen - returns string length
 * @s: string
@@ -29,14 +27,14 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 
-	m = malloc((size1 + size2) *sizeof(char) + 1);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (m == 0)
 		return (0);
