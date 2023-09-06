6a5c5b6336b2f2cdc79be001cefc7a8244c0908c
netweaver <developer.oduwa@gmail.com>
2023-09-06 23:51:35 +0100

    0x0B. C - malloc, free task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* alloc_grid - create pointers to pointers to int
+* @width: size of the array
+* @height: size of the element array
+* Return: nested array
+*/
+int **alloc_grid(int width, int height)
+{
+	int **tab, x, y;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (x = 0; x < height; x++)
+		{
+			if (tab[x] == 0)
+			{
+
+				tab[x] = malloc(sizeof(**tab) * width);
+				while (x++)
+					free(tab[x]);
+				free(tab);
+				return (NULL);
+			}
+
+			for (y = 0; y < width; y++)
+				tab[x][y] = 0;
+		}
+	}
+
+	return (tab);
+}
