c6658824188ebd1f415272bdd09c3b681ce218cc
netweaver <developer.oduwa@gmail.com>
2023-09-13 18:11:17 +0100

    0x0F. C - Function pointers task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,11 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (array && size && cmp)
+	{
 		while (i < size)
 		{
 			if (cmp(array[i]))
 				return (i);
 			i++;
 		}
-		return (-1);
+	}
+	return (-1);
 }
