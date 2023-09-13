8ab63e68419551c8b735dafcb0eed94235e63c7f
netweaver <developer.oduwa@gmail.com>
2023-09-13 17:52:08 +0100

    0x0F. C - Function pointers task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
 
+
 /**
 * print_name - prints a name
-* @name: the name to print as pointers to chars
-* @f: the function to print the name
+* @name: the string name
+* @f: the printing function pointer
+*
 * Return: void
 */
 
 void print_name(char *name, void (*f)(char *))
 {
+
 	if (name && f)
 		f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,21 @@
-#ifndef MAIN_H
+#ifndef FUNCTION_POINTERS_H
 
-#define MAIN_H
+#define  FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 
-
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
 #endif
