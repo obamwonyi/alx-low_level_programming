0e14bea464d8444dc0179bca168b6d33fad47783
netweaver <developer.oduwa@gmail.com>
2023-09-14 17:08:30 +0100

    0x10. C - Variadic functions task 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	while (i--)
-		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(ni)",
+		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
 			i ? (separator ? separator : "") : "\n");
 
 	va_end(ap);
