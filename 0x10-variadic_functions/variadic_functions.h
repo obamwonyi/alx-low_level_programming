d8d9cc3962d3d451b61cdf9089c9a37ab54e2bec
netweaver <developer.oduwa@gmail.com>
2023-09-14 11:25:44 +0100

    0x10. C - Variadic functions task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,17 @@
 
 #define VARIADIC_FUNCTIONS_H
 
+/**
+* struct token - struct token
+*
+* @token: format token
+* @f: the function associated
+*/
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 
