7bdc7f3b2bb40e4fba25e07f0e62cc077aded563
netweaver <developer.oduwa@gmail.com>
2023-09-23 02:08:50 +0100

    Add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+*add_node_end - adds a node to the end of a linked list
+*@head: address of the head node pointer
+*@str: str node filed
+*Return: size of the list
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !new_node)
+		return (NULL);
+
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
