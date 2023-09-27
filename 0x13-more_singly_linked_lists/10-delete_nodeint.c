0e744825f3fe6a52945056de9714489036bee407
netweaver <developer.oduwa@gmail.com>
2023-09-27 03:14:26 +0100

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	linstint_t *node, *prev_node;
+	listint_t *node, *prev_node;
 	unsigned int i = 0;
 
 	if (!head || !*head)
