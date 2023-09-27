c94c2471ed16ba6a94d2e83e65b6768898eee549
netweaver <developer.oduwa@gmail.com>
2023-09-27 01:06:40 +0100

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+*listint_len - prints the length of a linked list
+*@h: pointer to the very first node
+*Return: size of list
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
