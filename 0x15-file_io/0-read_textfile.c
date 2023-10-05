c88c1c35fac93d876b4db2b2caa2c6b75e2fb34b
netweaver <developer.oduwa@gmail.com>
2023-10-05 01:04:04 +0100

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,9 +15,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, &buf[0], bytes);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
 }
