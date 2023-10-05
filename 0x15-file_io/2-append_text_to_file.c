99e6bdce88ab8df584d75ad6e7960eb947bd53a8
netweaver <developer.oduwa@gmail.com>
2023-10-05 01:44:07 +0100

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,6 +37,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes = len ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
 
